62928c500b3caf1ce1dee841f82368b07c29a8c6
godricly <godricly.li@gmail.com>
2017-06-08 21:15:09 +0800

    iter implemented

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # zhihu_cup
-draft code for zhihu cup
+draft code for zhihu cup using MxNet</br>
 Code under construction
-
--- iter.py
+++ iter.py
@@ -1,6 +1,25 @@
+import os
 import numpy as np
 import mxnet as mx
 from read_embed import read_embed
+
+
+class SimpleBatch(object):
+    def __init__(self, data_names, data, label_names, label, bucket_key):
+        self.data = data
+        self.label = label
+        self.data_names = data_names
+        self.label_names = label_names
+        self.bucket_key = bucket_key
+
+    @property
+    def provide_data(self):
+        return [(n, x.shape) for n, x in zip(self.data_names, self.data)]
+
+    @property
+    def provide_label(self):
+        return [(n, x.shape) for n, x in zip(self.label_names, self.label)]
+
 class zhihu_iter(mx.io.DataIter):
     def __init__(self, question_set_path,
             question_topic,
@@ -8,7 +27,7 @@ class zhihu_iter(mx.io.DataIter):
             char_embed_path='./char_embedding.txt',
             word_embed_path='./word_embedding.txt',
             embed_mode = 0,
-            batch_size=1):
+            batch_size=32):
         with open(question_set_path) as f:
             self.raw_questions = f.readlines()
         with open(question_topic) as f:
@@ -19,7 +38,17 @@ class zhihu_iter(mx.io.DataIter):
         
         self.batch_size = batch_size
         self.embed_mode = embed_mode
-        self.create_buckets()
+
+        if os.path.exists('bucket_cache.txt'):
+            buckets = open('bucket_cache.txt').readlines()
+            buckets = [v.strip() for v in buckets]
+            self.create_buckets(buckets)
+        else:
+            self.create_buckets()
+            with open('bucket_cache.txt','w') as f:
+                for v in self.buckets:
+                    f.write(v+'\n')
+        self.reset()
         if embed_mode %2 == 0:
             self.char_dict, self.char_dict_size, self.char_dict_dim = read_embed(char_embed_path)
         if embed_mode > 0:
@@ -34,30 +63,46 @@ class zhihu_iter(mx.io.DataIter):
         else:
             self.buckets = buckets
             self.buckets_num = len(buckets)
-        self.bucket_samples_inds = [[] for _ in self.buckets]
+
+        bucket_filter = np.asarray([[int(v) for v in k.split(',')] for k in self.buckets])
+        self.bucket_samples_inds = {k:[] for k in self.buckets}
 
         for idx, v in enumerate(self.raw_questions):
             values = v.split()
             if len(values) < 3:
                 continue
-            tc = values[1].split(',')
-            tw = values[2].split(',')
+            tc = len(values[1].split(','))
+            tw = len(values[2].split(','))
             if len(values) > 3:
                 if values[3][0]=='c':
-                    cc = values[3].split(',')
+                    cc = len(values[3].split(','))
                 else:
                     cc = tc
-                    cw = values[3].split(',')
+                    cw = len(values[3].split(','))
                 if len(values) == 5:
-                    cw = values[4].split(',')
+                    cw = len(values[4].split(','))
             else:
                 cc = tc
                 cw = tw
-
-                    
             
-
-
+            if self.embed_mode % 2 == 0:
+                tc_bucket = min(np.where(bucket_filter[:,0] >= tc)[0])
+                cc_bucket = min(np.where(bucket_filter[:,1] >= cc)[0])
+                if self.embed_mode > 0:
+                    tw_bucket = min(np.where(bucket_filter[:,2] >= tw)[0])
+                    cw_bucket = min(np.where(bucket_filter[:,3] >=cw )[0])
+                    bucket_id = max([tc_bucket, cc_bucket, tw_bucket, cw_bucket])
+                else:
+                    bucket_id = max([tc_bucket, cc_bucket])
+            else:
+                tw_bucket = min(np.where(bucket_filter[:,0] >= tw)[0])
+                cw_bucket = min(np.where(bucket_filter[:,1] >= cw)[0])
+                bucket_id = max([tw_bucket, cw_bucket])
+            self.bucket_samples_inds[self.buckets[bucket_id]].append(idx)
+        batch_dist = np.asarray([len(v)/self.batch_size for v in self.bucket_samples_inds.values()])
+        self.max_bucket_key = self.buckets[max(np.where(batch_dist > 0)[0])]
+        self.bucket_order = np.hstack((np.ones(v,dtype=int)*i for i,v in enumerate(batch_dist)))
+    
     def default_buckets(self):
         self.buckets_num = 10
         tc_length = {}
@@ -105,104 +150,92 @@ class zhihu_iter(mx.io.DataIter):
 
         if self.embed_mode % 2 == 0:
             if self.embed_mode > 0:
-                self.max_bucket_key = ','.join([str(max_tc), str(max_cc), str(max_tw), str(max_cw)])
                 buckets = [','.join([str(max_tc*(i+1)/self.buckets_num),
                             str(max_cc*(i+1)/self.buckets_num),
                             str(max_tw*(i+1)/self.buckets_num),
                             str(max_cw*(i+1)/self.buckets_num)]) 
                             for i in range(self.buckets_num)]
             else:
-                self.max_bucket_key = ','.join([str(max_tc), str(max_cc)])
                 buckets = [','.join([str(max_tc*(i+1)/self.buckets_num),
                             str(max_cc*(i+1)/self.buckets_num)])
                             for i in range(self.buckets_num)]
         else:
-            self.max_bucket_key = ','.join([str(max_tw), str(max_cw)])
             buckets = [','.join([str(max_tw*(i+1)/self.buckets_num),
                         str(max_cw*(i+1)/self.buckets_num)]) 
                         for i in range(self.buckets_num)]
         return buckets
 
     def reset(self):
-        self.order = np.random.permutation(len(self.raw_questions))
+        self.bucket_order = np.random.permutation(self.bucket_order)
+        self.bucket_offset = [0 for _ in self.buckets]
+        for k,v in self.bucket_samples_inds.items():
+            self.bucket_samples_inds[k] = np.random.permutation(v)
+        
+
 
     def __iter__(self):
-        max_tc = 0
-        max_cc = 0
-        max_tw = 0
-        max_cw = 0
-        for idx in self.order:
-            raw_value = self.raw_questions[idx]
-            raw_label = self.question_topic[idx]
-            values = raw_value.split()
-            labels = raw_label.split()[1].split(',')
-            label = np.zeros((self.batch_size, len(self.topic_info) + 1))
-            for l in labels:
-                label[0, self.topic_encode[l]]=1
-            label_name =['topic']
-            label = [mx.nd.array(label)]
-            # print len(values)
+        for idx in self.bucket_order:
+            bucket_key = self.buckets[idx]
+            inds= self.bucket_samples_inds[bucket_key] \
+                    [self.bucket_offset[idx]:self.bucket_offset[idx]+self.batch_size]
 
-            title_char = values[1]
-            title_word = values[2]
-            if len(values) > 3:
-                if values[3][0]=='c':
-                    content_char = values[3]
-                else:
-                    content_word = values[3]
-                    content_char = title_char
-                if len(values) > 4:
-                    content_word = values[4]
-                else:
-                    content_word = title_word
+            shapes= [(self.batch_size, int(v)) for v in bucket_key.split(',')]
+            if len(shapes) == 4:
+                tc_array = np.zeros(shapes[0]+(self.char_dict_dim,))
+                cc_array = np.zeros(shapes[1]+(self.char_dict_dim,))
+                tw_array = np.zeros(shapes[2]+(self.word_dict_dim,))
+                cw_array = np.zeros(shapes[3]+(self.word_dict_dim,))
+            elif self.embed_mode  %2 == 0:
+                tc_array = np.zeros(shapes[0]+(self.char_dict_dim,))
+                cc_array = np.zeros(shapes[1]+(self.char_dict_dim,))
             else:
-                # duplicating title as content
-                content_char = title_char
-                content_word = title_word
+                tw_array = np.zeros(shapes[0]+(self.word_dict_dim,))
+                cw_array = np.zeros(shapes[1]+(self.word_dict_dim,))
+            
+            label = np.zeros((self.batch_size, len(self.topic_encode)+1))
+            for i,ind in enumerate(inds):
+                values = self.raw_questions[ind].split()
+                tc = values[1]
+                tw = values[2]
+                if len(values) > 3:
+                    if values[3][0]=='c':
+                        cc = values[3]
+                    else:
+                        cc = tc
+                        cw = values[3]
+                    if len(values) == 5:
+                        cw = values[4]
+                else:
+                    cc = tc
+                    cw = tw
 
-            title_char = title_char.split(',')
-            title_word = title_word.split(',')
-            content_char = content_char.split(',')
-            content_word = content_word.split(',')
-            data_name = []
-            data = []
-            bucket_key = ''
+                data_name = []
+                data = []
+                if self.embed_mode  %2 == 0:
+                    for j, v in enumerate(tc.split(',')):
+                        tc_array[i,j] = self.char_dict[v]
+                    for j, v in enumerate(cc.split(',')):
+                        cc_array[i,j] = self.char_dict[v]
+                    data_name += ['tc_array', 'cc_array']
+                    data += [mx.nd.array(tc_array), mx.nd.array(cc_array)]
+                if self.embed_mode > 0:
+                    for j, v in enumerate(tw.split(',')):
+                        tw_array[i,j] = self.word_dict[v]
+                    for j, v in enumerate(cw.split(',')):
+                        cw_array[i,j] = self.word_dict[v]
+                    data_name += ['tw_array', 'cw_array']
+                    data += [mx.nd.array(tw_array), mx.nd.array(cw_array)]
 
-
-            if self.embed_mode  %2 == 0:
-                tc_array = np.zeros((len(title_char), self.char_dict_dim))
-                cc_array = np.zeros((len(content_char), self.char_dict_dim))
-                max_tc = max(max_tc, len(title_char))
-                max_cc = max(max_cc, len(content_char))
-                for i, c in enumerate(title_char):
-                    tc_array[i,:] = self.char_dict[c]
-                for i, c in enumerate(content_char):
-                    cc_array[i,:] = self.char_dict[c]
-                data_name += ['tc_array', 'cc_array']
-                data += [mx.nd.array(tc_array), mx.nd.array(cc_array)]
-                bucket_key += str(tc_array.shape[0])+','+str(cc_array.shape[0])
-                # print tc_array.shape, cc_array.shape
-
-            if self.embed_mode > 0:
-                tw_array = np.zeros((len(title_word), self.word_dict_dim))
-                cw_array = np.zeros((len(content_word), self.word_dict_dim))
-                max_tw = max(max_tw,len(title_word))
-                max_cw = max(max_cw,len(content_word))
-                for i, w in enumerate(title_word):
-                    tw_array[i,:] = self.word_dict[w]
-                for i, w in enumerate(content_word):
-                    cw_array[i,:] = self.word_dict[w]
-                data_name += ['tw_array', 'cw_array']
-                data += [mx.nd.array(tw_array), mx.nd.array(cw_array)]
-                if len(bucket_key) > 0:
-                    bucket_key += ','
-                bucket_key += str(tw_array.shape[0])+','+str(cw_array.shape[0])
-                # print tw_array.shape, cw_array.shape
-        print max_tc,max_cc,max_tw,max_cw
-
-
-
+                top = self.question_topic[ind].split()[1].split(',')
+                for t in top:
+                    label[i,self.topic_encode[t]] = 1
+            label = [mx.nd.array(label)]
+            label_name = ['label']
+            yield SimpleBatch(data_name, data, label_name, label, bucket_key)
+        raise StopIteration
 
 if __name__ == '__main__':
     ziter = zhihu_iter('tidy_question_train_set.txt','tidy_question_topic_train_set.txt',embed_mode=1)
-    ziter.reset()
+    #ziter.reset()
+    for i in ziter:
+        print i
